f29dcfd008ee6f60420208c2b407bde270bb90c2
Damien Biasotto <damien@occi-tech.com>
2013-10-18 10:20:58 +0200

    Remove authcomponent declaration from plugin. closes #4870

--- Controller/InvoicesAppController.php
+++ Controller/InvoicesAppController.php
@@ -1,38 +1,7 @@
 <?php
 App::uses('AppController', 'Controller');
 class InvoicesAppController extends AppController {
-/**
- * Components
- *
- * @var array
- */
-	public $components = array(
-		'Auth',
-		'Session',
-		'RequestHandler'
-	);
-
-/**
- * Helpers
- *
- * @var array
- */
-	public $helpers = array(
-		'Form',
-		'Html',
-		'Session',
-	);
-
-	public function beforeRender() {
-		if (method_exists($this, '_isAdminArea')) {
-			$isAdminArea = $this->_isAdminArea();
-		} else {
-			$isAdminArea = !empty($this->request->params['prefix']) && $this->request->params['prefix'] === 'admin';
-		}
-
-		$this->set('isAdminArea', $isAdminArea);
-		parent::beforeRender();
-	}
+	public $components = array('RequestHandler');
 }
 
 ?>
